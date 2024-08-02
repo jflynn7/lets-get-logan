0c7603081d9e1ad92597bb39e23eaa49e4a82fef
Joe Flynn <joe@prettyfly.it>
2024-08-02 17:09:18 +0100

    [INVOICE] Adds form

--- app/components/Invoice.tsx
+++ app/components/Invoice.tsx
@@ -15,14 +15,36 @@ export interface InvoicingCompany {
     email: string;
 }
 
+
 export interface InvoiceProps {
     invoicingCompany: InvoicingCompany;
-    date: Date;
+    invoiceRecipient: InvoiceRecipient;
+    invoicedJob: InvoiceJobDetails;
+    jobItems: JobItem[];
+    date: string;
+    paymentDetails: PaymentDetails;
+}
+
+export interface InvoiceJobDetails {
+    jobName: string;
+    jobAddress: string;
+    workOrderNumber: string
+}
+
+export interface JobItem {
+    description: string;
+    cost: number;
+}
+
+export interface PaymentDetails {
+    bankName: string;
+    accountNumber: string;
+    invoiceReference: string;
 }
 export default function Invoice(invoiceProps: InvoiceProps): JSX.Element {
-    const { invoicingCompany, date } = invoiceProps;
+    const { invoicingCompany, paymentDetails, jobItems, invoiceRecipient, invoicedJob , date } = invoiceProps;
     return <>
-        <div className="invoice max-w-2xl mx-auto p-6 bg-white text-black shadow-lg">
+        <div id={'generated-invoice'} className="invoice max-w-2xl mx-auto p-6 bg-white text-black shadow-lg">
             <div className="flex">
                 <div className="w-1/4">
                     <Image alt={'LPM Logo'} src={'/lpm-logo.jpg'} width={800} height={800} />
@@ -30,58 +52,59 @@ export default function Invoice(invoiceProps: InvoiceProps): JSX.Element {
                 <div className="w-3/4 left-1/2 text-right">
                     <h1 className="text-3xl font-bold">Logan Property Maintenance</h1>
                     <p>{invoicingCompany.companyAddress}</p>
-                    <p>Phone: {invoicingCompany.phoneNumber}</p>
-                    <p>Email: {invoicingCompany.email}</p>
-                    <p>Date: {date.toLocaleDateString()}</p>
+                    <p><strong>Phone:</strong> {invoicingCompany.phoneNumber}</p>
+                    <p><strong>Email:</strong> {invoicingCompany.email}</p>
+                    <p><strong>Date:</strong> {date}</p>
                 </div>
             </div>
             <div className="flex mt-6">
                 <div className="w-1/2">
                     <h2 className="text-lg font-bold">To:</h2>
-                    <p>Recipient Name</p>
-                    <p>Recipient Address</p>
-                    <p>Phone: 987-654-3210</p>
-                    <p>Email: recipient@example.com</p>
+                    <p><strong>{invoiceRecipient?.name}</strong></p>
+                    <p>{invoiceRecipient?.address}</p>
+                    { invoiceRecipient?.phone && <p><strong>Phone:</strong> {invoiceRecipient.phone}</p>}
+                    { invoiceRecipient?.email && <p><strong>Email:</strong> {invoiceRecipient.email}</p>}
                 </div>
                 <div className="w-1/2">
                     <h2 className="text-lg font-bold">For:</h2>
-                    <p>Job Site Name</p>
-                    <p>Job Site Address</p>
-                    <p>Work Order Number: WO123456</p>
+                    <p>{invoicedJob?.jobName}</p>
+                    <p>{invoicedJob?.jobAddress}</p>
+                    <p><strong>Work Order Number:</strong> {invoicedJob?.workOrderNumber}</p>
                 </div>
             </div>
             <div className="invoice-details mt-6">
                 <table className="w-full">
                     <tbody>
+
                     <tr>
                         <th className="bg-gray-100 p-4 text-left">Description</th>
                         <th className="bg-gray-100 p-4 text-right">Amount</th>
                     </tr>
-                    <tr>
-                        <td className="p-4 text-left">Item 1</td>
-                        <td className="p-4 text-right">$10.00</td>
-                    </tr>
-                    <tr>
-                        <td className="p-4 text-left">Item 2</td>
-                        <td className="p-4 text-right">$5.00</td>
-                    </tr>
+                    {jobItems?.map((item: JobItem, index: number) => {
+                        return <tr key={index}>
+                            <td className="p-4 text-left">{item.description}</td>
+                            <td className="p-4 text-right">£{item.cost}</td>
+                        </tr>
+                    })}
                     <tr>
                         <td className="font-bold p-4 text-left">Total</td>
-                        <td className="font-bold p-4 text-right">$15.00</td>
+                        <td className="font-bold p-4 text-right">£{jobItems?.reduce((total: number, item: JobItem) => {
+                            total += Number(item.cost);
+                            return total;
+                        }, 0)}</td>
                     </tr>
                     </tbody>
                 </table>
             </div>
             <div className="payment-details mt-6">
                 <h2 className="text-lg font-bold">Payment Details:</h2>
-                <p>Bank: Your Bank Name</p>
-                <p>Account Number: 1234567890</p>
-                <p>Invoice Reference: INV123456</p>
+                <hr className="m-2"/>
+                <p><strong>Bank:</strong> Your Bank Name</p>
+                <p><strong>Account Number:</strong> 1234567890</p>
+                <p><strong>Sort Code:</strong> 12-34-56</p>
+                <hr className="m-2"/>
+                <p><strong>Invoice Reference:</strong> INV123456</p>
             </div>
         </div>
-
-        <button className="hidden-print bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
-                onClick={() => window.print()}>Print Invoice
-        </button>
     </>;
 }
--- app/invoice/page.tsx
+++ app/invoice/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import Invoice, {InvoiceProps} from "@/app/components/Invoice";
+import InvoiceForm from "@/app/components/InvoiceForm";
+import {useState} from "react";
 
 export default function InvoicePage() {
     const defaultProps: InvoiceProps = {
@@ -9,12 +11,30 @@ export default function InvoicePage() {
             phoneNumber: '07434345342',
             email: 'richard@letsgetlogan.com',
         },
-        date: new Date(),
+        date: new Date().toDateString(),
+    }
+
+    const [invoiceProps, setInvoiceProps] = useState(defaultProps)
+
+    const handlePrint = () => {
+        const printContent = document.getElementById('generated-invoice')?.innerHTML || "";
+        const originalContent = document.body.innerHTML;
+        document.body.innerHTML = printContent;
+        window.print();
+        document.body.innerHTML = originalContent;
     }
     return <>
         <div>
+            <p className="text-xl font-bold text-center p-3">Create Invoice</p>
+            <InvoiceForm onSubmit={setInvoiceProps} />
             <p className="text-xl font-bold text-center p-3">Invoice Preview</p>
-            <Invoice {...defaultProps} />
+            <Invoice {...invoiceProps} />
+            <div className="content-center">
+                <button
+                    className="hidden-print bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
+                    onClick={handlePrint}>Print Invoice
+                </button>
+            </div>
         </div>
     </>
 }
